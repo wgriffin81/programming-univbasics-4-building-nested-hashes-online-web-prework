b38b4b78198c5d6334096d9699a30d982f55909c
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-19 01:11:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,11 +28,10 @@ def monopoly_with_third_tier
    monopoly = {
      :railroads => {
 	    :pieces => 4,
-	    :rent_in_dollars =>{
-	      :one_piece_owned=>{},
-        :two_pieces_owned=>{},
-        :three_pieces_owned=>{},
-        :four_pieces_owned=>{}	
+	    :rent_in_dollars => {
+	      :names => {
+	        
+	      }
 	    }
 	  }
 	  
